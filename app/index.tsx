c0cf60c6a99a4b4f05e20093bcae586b65eca57f
bacowan <bacowan@ualberta.ca>
2024-06-16 23:24:02 +0900

    Start of a home page

--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,37 @@
-import { Text, View } from "react-native";
+import { View } from "react-native";
+import { Stack, Link } from 'expo-router';
+import { Button, useTheme } from 'react-native-paper';
 
 export default function Index() {
+  const theme = useTheme();
+
   return (
     <View
       style={{
         flex: 1,
+        rowGap: 30,
         justifyContent: "center",
         alignItems: "center",
-      }}
-    >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
+        backgroundColor: theme.colors.background
+      }}>
+      <Stack.Screen
+        options={{
+          title: "Spin Alone"
+        }}/>
+      <Link href="./play" asChild>
+        <Button onPress={() => {}}
+          mode="contained-tonal"
+          buttonColor={theme.colors.primary}
+          textColor={theme.colors.onPrimary}>
+              Start
+          </Button>
+      </Link>
+      <Button onPress={() => {}}
+        mode="contained-tonal"
+        buttonColor={theme.colors.secondary}
+        textColor={theme.colors.onSecondary}>
+            Edit
+        </Button>
     </View>
   );
 }
---
+++ app/play.tsx
@@ -0,0 +1,5 @@
+import { Text, StyleSheet } from "react-native";
+
+export default function Play() {
+    return <Text>Hi</Text>
+}
